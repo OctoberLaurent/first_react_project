9725a5e32b218bec5bb0ca51465d3c4e0640f502
OctoberLaurent <43971010+OctoberLaurent@users.noreply.github.com>
2019-08-05 21:41:32 +0200

    last change

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import Client from "./Client";
+import ClientForm from "./ClientForm";
 
 import "./styles.css";
 
@@ -9,8 +11,7 @@ class App extends React.Component {
       { id: 1, nom: "Laurent LEPLAT" },
       { id: 2, nom: "Orhan Topak" },
       { id: 3, nom: "Bruno Paris" }
-    ],
-    nouveauClient: ""
+    ]
   };
 
   handleDelete = id => {
@@ -20,18 +21,11 @@ class App extends React.Component {
     this.setState({ clients });
   };
 
-  handleSubmit = event => {
-    event.preventDefault();
-    const id = new Date().getTime();
-    const nom = this.state.nouveauClient;
+  handleAdd = client => {
     const clients = [...this.state.clients];
-    clients.push({ id, nom });
-    this.setState({ clients, nouveauClient: "" });
+    clients.push(client);
+    this.setState({ clients });
   };
-  handleChange = event => {
-    this.setState({ nouveauClient: event.currentTarget.value });
-  };
-
   render() {
     const title = "Liste des clients";
 
@@ -40,21 +34,10 @@ class App extends React.Component {
         <h1>{title}</h1>
         <ul>
           {this.state.clients.map(client => (
-            <li>
-              {client.nom}
-              <button onClick={() => this.handleDelete(client.id)}>X</button>
-            </li>
+            <Client details={client} onDelete={this.handleDelete} />
           ))}
         </ul>
-        <form onSubmit={this.handleSubmit}>
-          <input
-            value={this.state.nouveauClient}
-            onChange={this.handleChange}
-            type="text"
-            placeholder="Ajouter un nom"
-          />
-          <button>Envoyer</button>
-        </form>
+        <ClientForm onClientAdd={this.handleAdd} />
       </div>
     );
   }
